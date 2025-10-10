5a5d0155990df99e91a3e0173e4a5036b6630e5c
weisonweileen <weisonweileen@gmail.com>
2025-10-10 09:40:05 +0000

    update collision hardcoding
    Signed-off-by: weisonweileen <weisonweileen@gmail.com>

--- metasim/sim/isaacsim/isaacsim.py
+++ metasim/sim/isaacsim/isaacsim.py
@@ -599,7 +599,7 @@ class IsaacsimHandler(BaseSimHandler):
                     solver_position_iteration_count=4,
                     solver_velocity_iteration_count=0,
                 ),
-                collision_props=sim_utils.CollisionPropertiesCfg(collision_enabled=False),
+                collision_props=sim_utils.CollisionPropertiesCfg(collision_enabled=True),
             ),
             actuators={
                 # jn: ImplicitActuatorCfg(
