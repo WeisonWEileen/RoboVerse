aed4a4e5e8a6cce2c2c91e0741c6de8e018d5bd2
WeisonWeileen <weisonweileen@gmail.com>
2025-10-31 22:37:41 -0700

    delete useless
    Signed-off-by: WeisonWeileen <weisonweileen@gmail.com>

--- humanoid_visualrl/wrapper/active_vision_cube_wrapper.py
+++ humanoid_visualrl/wrapper/active_vision_cube_wrapper.py
@@ -782,13 +782,13 @@ class ActiveVisionWrapper(HumanoidBaseWrapper):
             # update curriculum_cube_yaw_range
             # Check curriculum update every 100 iterations (not steps) to prevent too frequent updates
             current_iteration = int(self.common_step_counter / self.cfg.ppo_cfg.num_steps_per_env)
+            # if current_iteration < 400:
+            #     return
+
+            # if current_iteration < 250:
+            #      return
+
             if current_iteration < 400:
-                return
-
-            if current_iteration < 250:
-                 return
-
-            if current_iteration < 250:
                  return
 
 
